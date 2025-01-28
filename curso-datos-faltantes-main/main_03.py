bb9bb46e43c19c174cc1506a4c209c35c3ce7ff5
Francisco Cisneros <fcisnerosr@outlook.es>
2025-01-28 07:13:33 -0600

    comandos para tabluar valores faltantes

---
+++ curso-datos-faltantes-main/main_03.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from bases_02 import riskfactors_df
+import pandas_missing_extension
+import os
+
+#  os.system('clear')
+
+# Tabulación de valores faltantes
+#  print(riskfactors_df.isna())
+
+# Resuenes basicos de valores faltantes
+print(riskfactors_df.shape)
+valores_completos = riskfactors_df.missing.number_complete()
+print(f'Valores completos = {valores_completos}')
+valores_faltantes = riskfactors_df.missing.number_missing()
+print(f'Valores faltantes = {valores_faltantes}')
+
+# Resúmenes tabulares de valores faltantes
+# Variables/Columnas
+# Resumen por variable
+#  print(riskfactors_df.missing.missing_variable_summary()) #   Genera una tabla con información sobre valores faltantes: número de faltantes, total de observaciones y porcentaje por cada columna.
+print(riskfactors_df.missing.missing_variable_table())  # Imprime una tabla que agrupa variables por la cantidad de valores faltantes que contienen y su porcentaje.
+print((
+    riskfactors_df
+    .missing
+    .missing_variable_span(
+        variable='weight_lbs',
+        span_every=50
+    )
+)) 
+# Genera un análisis de valores faltantes en un DataFrame, dividiendo los datos en intervalos definidos.
+# Para cada intervalo, la función calcula:
+#   - La cantidad de valores faltantes.
+#   - La cantidad de valores completos.
+#   - El porcentaje de completitud (valores completos / total de valores)
+# La función devuelve una tabla resumen que facilita la identificación de patrones de datos faltantes en el DataFrame.
+print((
+    riskfactors_df
+    .missing
+    .missing_variable_run(
+        variable='weight_lbs',
+    )
+))
+# Crea una tabla con información sobre las rachas de datos faltantes y completos en una columna. 
+# La tabla muestra, para cada racha:
+#   - El tipo de racha (faltante o completo).
+#   - La longitud de la racha actual.
