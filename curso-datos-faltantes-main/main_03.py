870ced34cccacdce2791bae49255537a841fb8e9
Francisco Cisneros <fcisnerosr@outlook.es>
2025-01-30 21:25:17 -0600

    datos faltantes

--- curso-datos-faltantes-main/main_03.py
+++ curso-datos-faltantes-main/main_03.py
@@ -18,22 +18,28 @@ print(f'Valores faltantes = {valores_faltantes}')
 # Resúmenes tabulares de valores faltantes
 # Variables/Columnas
 # Resumen por variable
-#  print(riskfactors_df.missing.missing_variable_summary()) #   Genera una tabla con información sobre valores faltantes: número de faltantes, total de observaciones y porcentaje por cada columna.
+print(riskfactors_df.missing.missing_variable_summary()) 
+# Realiza un análisis de valores faltantes a nivel de columna en el DataFrame.
+# La tabla resultante proporciona, para cada variable:
+#   - El número de valores considerados como faltantes.
+#   - La cuenta total de observaciones en la columna.
+#   - El porcentaje que representan los valores faltantes en la columna.
+
 print(riskfactors_df.missing.missing_variable_table())  # Imprime una tabla que agrupa variables por la cantidad de valores faltantes que contienen y su porcentaje.
-print((
-    riskfactors_df
-    .missing
-    .missing_variable_span(
-        variable='weight_lbs',
-        span_every=50
-    )
-)) 
+#  print((
+#      riskfactors_df
+#      .missing
+#      .missing_variable_span(
+#          variable='weight_lbs',
+#          span_every=50
+#      )
+#  ))
 # Genera un análisis de valores faltantes en un DataFrame, dividiendo los datos en intervalos definidos.
 # Para cada intervalo, la función calcula:
 #   - La cantidad de valores faltantes.
 #   - La cantidad de valores completos.
 #   - El porcentaje de completitud (valores completos / total de valores)
-# La función devuelve una tabla resumen que facilita la identificación de patrones de datos faltantes en el DataFrame.
+#  # La función devuelve una tabla resumen que facilita la identificación de patrones de datos faltantes en el DataFrame.
 print((
     riskfactors_df
     .missing
